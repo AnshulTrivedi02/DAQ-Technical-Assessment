f310fe4c7d0bad525945c554bf952150b2944f8a
AnshulTrivedi02 <63908790+AnshulTrivedi02@users.noreply.github.com>
2023-03-02 19:23:10 +1100

    Update server.ts
    Fixed the crashing of the streamed data, due to an extra curly bracket "}"

--- streaming-service/src/server.ts
+++ streaming-service/src/server.ts
@@ -11,10 +11,23 @@ tcpServer.on('connection', (socket) => {
     
     socket.on('data', (msg) => {
         console.log(msg.toString());
+        
 
         // HINT: what happens if the JSON in the received message is formatted incorrectly?
         // HINT: see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
-        let currJSON = JSON.parse(msg.toString());
+        try {
+            console.log(msg.toString());
+            let ReceivedMsg = msg.toString(); //making the buffer into a string
+            console.log("This is the sliced string"+msg.toString().slice(0,-1));
+            if(ReceivedMsg.endsWith("}}")) { //checking the format of the JSON
+                let NewMsg = ReceivedMsg.slice(0,-1); // slicing last bracket to fix JSON syntax
+                ReceivedMsg = NewMsg //storing the correct msg JSON syntax into the message
+            }
+
+            let currJSON = JSON.parse(ReceivedMsg); //parsing the received messsage
+        }
+        catch(error1){
+        console.error(error1);}
 
         websocketServer.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
