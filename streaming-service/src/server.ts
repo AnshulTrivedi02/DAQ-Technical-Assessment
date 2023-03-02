ef382b3f59403e03fd1a6438409b2f7d0bda8b11
AnshulTrivedi02 <63908790+AnshulTrivedi02@users.noreply.github.com>
2023-03-02 21:40:52 +1100

    Update server.ts
    Fixing some task 1 and writing some basic logic for task 2

--- streaming-service/src/server.ts
+++ streaming-service/src/server.ts
@@ -6,25 +6,37 @@ const TCP_PORT = parseInt(process.env.TCP_PORT || '12000', 10);
 const tcpServer = net.createServer();
 const websocketServer = new WebSocketServer({ port: 8080 });
 
+const fs = require('fs');
+
 tcpServer.on('connection', (socket) => {
     console.log('TCP client connected');
     
     socket.on('data', (msg) => {
         console.log(msg.toString());
-        
 
         // HINT: what happens if the JSON in the received message is formatted incorrectly?
         // HINT: see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
         try {
             console.log(msg.toString());
             let ReceivedMsg = msg.toString(); //making the buffer into a string
-            console.log("This is the sliced string"+msg.toString().slice(0,-1));
+            // console.log("This is the sliced string"+msg.toString().slice(0,-1));
             if(ReceivedMsg.endsWith("}}")) { //checking the format of the JSON
                 let NewMsg = ReceivedMsg.slice(0,-1); // slicing last bracket to fix JSON syntax
                 ReceivedMsg = NewMsg //storing the correct msg JSON syntax into the message
             }
 
             let currJSON = JSON.parse(ReceivedMsg); //parsing the received messsage
+
+            if ((currJSON.battery_temperature > 80) || (currJSON.battery_temperature < 20)) {
+                let TempTime = Date.now()/1000; //time in seconds
+                // let myfile = open('Battery-Temp.txt','r');
+                fs.appendFileSync("Battery-Temp.txt", TempTime); //append time to the file
+
+                // write the time at which the battery exceded the temperature ot a file
+                // when this if statement is accessed again, read the third last line (time),
+                // (which would now be the third newest time) in the file and then compare it with the newest time (which we just received).
+                // subtract the newwest time we just received and the now third newest time from the file and check whether 5 seconds has elapsed
+            }
         }
         catch(error1){
         console.error(error1);}
